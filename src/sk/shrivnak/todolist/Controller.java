0fff09c3a5c543bda9191b151a362f8580f77d37
SamuelHrivnak <samuel.hrivnak@gmail.com>
2017-11-17 23:50:03 +0100

    first commit

---
+++ src/sk/shrivnak/todolist/Controller.java
@@ -0,0 +1,4 @@
+package sk.shrivnak.todolist;
+
+public class Controller {
+}
--- src/sk/shrivnak/todolist/Main.java
+++ src/sk/shrivnak/todolist/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package sk.shrivnak.todolist;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+        primaryStage.setTitle("ToDoList");
+        primaryStage.setScene(new Scene(root, 760, 650));
         primaryStage.show();
     }
 
